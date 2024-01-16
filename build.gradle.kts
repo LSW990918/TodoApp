cd90743cee71740cc83c721b76c79ffb09e3f62e
LSW990918 <leesywon99@gmail.com>
2024-01-16 14:51:21 +0900

    StopWatch 어노테이션 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ allOpen {
 }
 
 dependencies {
-
+    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
---
+++ src/main/kotlin/com/example/mytodoapp/infra/swagger/aop/StopWatch.kt
@@ -0,0 +1,5 @@
+package com.example.courseregistration.infra.aop
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class StopWatch()
